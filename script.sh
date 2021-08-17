d52c12b54f19e66e0130b5ad2f3b4f5987151a63
DuBro <mihwarrior@gmail.com>
2021-08-17 14:44:29 +1000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-
 LOG='/root/smartfe-archive.log'
-SRC="/backup/tmp/"
+SRC="/backup/"
 DST="/cache/"
 PREFIX="-enc.7z"
-for i in `ls $SRC | grep "Smart"`
-do 
+for i in `ls $SRC | grep Smartfe| grep -v "enc" | grep -v "md5"`
+do
   ARCHNAME=$DST$i$PREFIX
-  echo "Copying $SRC$i to local storage /cache ..." >>$LOG 2>&1
+  echo "Copying $SRC$i to local storage /ca ..." >>$LOG 2>&1
   cp $SRC$i $DST$i
+  cp $SRC$i.md5 $DST$i.md5
+
   echo "Archiving $SDT$i ... " >>$LOG 2>&1
-  7za -mx0 a $ARCHNAME $DST$i -p$1
+  7za -mx0 a $ARCHNAME $DST$i $DST$i.md5 -p$1
   echo "Revoming tmpfile" >>$LOG 2>&1
-  rm $DST$i
+  rm $DST$i $DST$i.md5
   echo "Moving encrypted arch to S3" >>$LOG 2>&1
   mv $ARCHNAME $SRC
   echo "Done" >>$LOG 2>&1
